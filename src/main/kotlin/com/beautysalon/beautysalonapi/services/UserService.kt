b017e1383692dd89d4dde3701a13b62a2cc6ce31
Rafael <rafael.goncalves@tinnova.com.br>
2022-08-25 16:27:36 -0300

    add swagger

--- src/main/kotlin/com/beautysalon/beautysalonapi/services/UserService.kt
+++ src/main/kotlin/com/beautysalon/beautysalonapi/services/UserService.kt
@@ -3,9 +3,7 @@ package com.beautysalon.beautysalonapi.services
 import com.beautysalon.beautysalonapi.data.dtos.UserDTO
 
 interface UserService {
-
     fun createUser(userDTO: UserDTO): UserDTO
     fun getUsers(): List<UserDTO>
     fun getUser(id: Long): UserDTO
-
 }
